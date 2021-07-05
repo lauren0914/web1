f15ba0577516e650b38bb1ca364dabee90332ff8
lauren0914 <cofks05@likelion.org>
2021-07-05 16:34:28 +0900

    7/5 2ban class commit 1

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 def hello_world(request):
     # return HttpResponse('Hello World!')
-    return render(request, 'base.html')
+    return render(request, 'accountapp/hello_world.html')
 # request : 요청 관련 정보가 들어오는 곳. 지금은 안 씀
 # template name
