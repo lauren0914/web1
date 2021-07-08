df36142011561a5251ac7e796d616e1dc615749d
lauren0914 <cofks05@likelion.org>
2021-07-08 17:13:12 +0900

    7/8 2ban class commit 2

--- accountapp/views.py
+++ accountapp/views.py
@@ -5,6 +5,12 @@ from django.shortcuts import render
 
 def hello_world(request):
     # return HttpResponse('Hello World!')
-    return render(request, 'accountapp/hello_world.html')
+    if request.method == 'POST':
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'POST METHOD!'})
+    else:
+        return render(request, 'accountapp/hello_world.html',
+                      context={'text': 'GET METHOD!'})
+
 # request : 요청 관련 정보가 들어오는 곳. 지금은 안 씀
 # template name
