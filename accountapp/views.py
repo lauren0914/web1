9a470d3a2c39d71bb93c350a2e7a760c9a91f50b
lauren0914 <cofks05@likelion.org>
2021-08-02 18:04:06 +0900

    8/2 2ban class commit 4

--- accountapp/views.py
+++ accountapp/views.py
@@ -133,3 +133,6 @@ class AccountDeleteView(DeleteView):
     #         else:
     #             return HttpResponseForbidden()
 
+
+
+
--- articleapp/views.py
+++ articleapp/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from articleapp.forms import ArticleCreationForm
+from articleapp.models import Article
+
+
+class ArticleCreateView(CreateView):
+    model = Article
+    form_class = ArticleCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'articleapp/create.html'
+
+    # ????? 뭐가 문제? 
+    def form_valid(self, form):
+        # 검증 완료된 form의 instance에 접근해서 user를 wirter 에 할당
+        form.instance.writer = self.request.user
+        return super().form_valid(form)
