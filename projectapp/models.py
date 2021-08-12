d50bad57fcadfa2143e86adbacaa8b40ea0fb732
lauren0914 <cofks05@likelion.org>
2021-08-12 17:43:37 +0900

    8/12 2ban class commit 4

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    # null은 입력하고 말고를 정하는 게 아니야. (그건 blank.) 서버에 null을 허용하느냐 마냐의 문제임
+    description = models.CharField(max_length=200, null=True)
+    image = models.ImageField(upload_to='project/', null=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
