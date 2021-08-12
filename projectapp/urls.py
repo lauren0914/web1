64f28e08887ff5d726cb720468a09e6728515fe3
lauren0914 <cofks05@likelion.org>
2021-08-12 18:00:23 +0900

    8/12 2ban class commit 5

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
 
+from projectapp.views import ProjectCreateView
 
+app_name = 'projectapp'
+# revsere('projectapp:create') 이런 거 하려면.
 
 urlpatterns = [
+    path('create/', ProjectCreateView.as_view(), name='create'),
 
 ]
