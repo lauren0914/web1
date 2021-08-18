3ffcafbab7c634a8d7e5b192a7fa1e76b082d48d
lauren0914 <cofks05@likelion.org>
2021-08-18 12:25:46 +0900

    8/18 2ban class commit 2

--- projectapp/urls.py
+++ projectapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from projectapp.views import ProjectCreateView, ProjectDetailView
+from projectapp.views import ProjectCreateView, ProjectDetailView, ProjectListView
 
 app_name = 'projectapp'
 # revsere('projectapp:create') 이런 거 하려면.
@@ -8,4 +8,6 @@ app_name = 'projectapp'
 urlpatterns = [
     path('create/', ProjectCreateView.as_view(), name='create'),
     path('detail/<int:pk>', ProjectDetailView.as_view(), name='detail'),
+    path('list/', ProjectListView.as_view(), name='list')
+
 ]
