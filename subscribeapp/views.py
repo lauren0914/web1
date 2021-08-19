599acc5bdb9b276fa5da7726cefcea40435641d7
lauren0914 <cofks05@likelion.org>
2021-08-19 21:27:17 +0900

    8/19 2ban class commit 6

--- subscribeapp/views.py
+++ subscribeapp/views.py
@@ -1,3 +1,29 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+from django.utils.decorators import method_decorator
+from django.views.generic import RedirectView
+
+from projectapp.models import Project
+from subscribeapp.models import Subscription
+
+@method_decorator(login_required, 'get')
+class SubscriptionView(RedirectView):
+    # get 요청을 처리해주는 http 머시기
+    def get(self, request, *args, **kwargs):
+        # user가 누구인지. 어떤 게시판의 구독정보를 요청했는지가 필요함
+        user = request.user
+        project = Project.objects.get(pk=kwargs['project_pk'])
+
+        subscription = Subscription.objects.filter(user=user, project=project)
+        # 구독돼있으면 구독취소
+        if subscription.exists():
+            subscription.delete()
+        else:
+            Subscription(user=user, project=project).save()
+        return super().get(request, *args, **kwargs)
+
+    def get_redirect_url(self, *args, **kwargs):
+        return reverse('projectapp:detail', kwargs={'pk':kwargs['project_pk']})
