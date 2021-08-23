0fc9083d2f638d9ee2f3085030e9c20a1c7c07f9
lauren0914 <cofks05@likelion.org>
2021-08-23 16:58:16 +0900

    8/23 2ban class commit 1

--- projectapp/views.py
+++ projectapp/views.py
@@ -10,6 +10,8 @@ from django.views.generic.list import MultipleObjectMixin
 from articleapp.models import Article
 from projectapp.forms import ProjectCreationForm
 from projectapp.models import Project
+from subscribeapp.models import Subscription
+
 
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
@@ -30,10 +32,26 @@ class ProjectDetailView(DetailView, MultipleObjectMixin):
     paginate_by = 20
 
     # 템플릿에서 사용할 수 있도록 필터링..?
+    # 템플릿에서 사용하는 문맥 데이터를 만들어주기 위한 메소드임. -> 구독여부 확인하는 로직 넣어주기
     def get_context_data(self, **kwargs):
+        user = self.request.user
+        # target_project랑 동일
+        project = self.object
+
+        # user가 로그인 되어있는지 확인. 원래 method ~ 하지 않았나요?
+        # 그렇게 되면 문제가. 로그인 된 유저만 게시글을 볼 수 있잖아. 우리는 로그인 안 된 사람도 게시글 볼 수 있게 만들자
+        if user.is_authenticated:
+            subscription = Subscription.objects.filter(user=user,
+                                                       project=project)
+        else:
+            subscription = None
+        # -> subscription 에 구독여부를 확인할 수 있는 데이터가 들어있을 거야
+
         # 조건에 맞는 애들만 필터링하겠다. self.object = target_project
         article_list = Article.objects.filter(project=self.object)
-        return super().get_context_data(object_list=article_list, **kwargs)
+        return super().get_context_data(object_list=article_list,
+                                        subscription=subscription,
+                                        **kwargs)
 
 
 class ProjectListView(ListView):
