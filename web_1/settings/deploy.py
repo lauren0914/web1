778270d9a00a7b0aa74da941d417dca291514b4d
lauren0914 <cofks05@likelion.org>
2021-09-23 17:44:55 +0900

    09/23 2ban class commit 2

--- web_1/settings/deploy.py
+++ web_1/settings/deploy.py
@@ -1,27 +1,17 @@
 from .base import *
 # base 파일에서 생성했던 모든 것을 가져오기
 
-env_list = dict()
-local_env = open(os.path.join(BASE_DIR, '.env'))
-# os : 운영체제 경로에 관련된 모듈이 들어가있는 게 os.path 임/ join : 합친다. base_dir + env
-
-while True:
-    line = local_env.readline()
-    if not line:
-        break
-    line = line.replace('\n', '')
-    # 줄이 여러개 넣는다고 하면 줄바꿈줄이 항상 포함될 것이기 때문에 그건 실제 밸류가 아니기 때문에 없애줌
-    start = line.find('=')
-    # 처음 만난 값의 인덱스를 돌려줌. = 를 기준으로 슬라이싱하기 위해서.
-    key = line[:start]
-    value = line[start+1:]
-    env_list[key] = value
-
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip() # 혹시나 있을지도 모르는 좌우 공백 제거
+    file.close()
+    return secret
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env_list['SECRET_KEY']
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -37,8 +27,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
+        'USER': read_secret('MARIADB_USER'),
+        'PASSWORD': read_secret('MARIADB_PASSWORD'),
         'HOST': 'mariadb',
         'PORT': '3306',
     }
